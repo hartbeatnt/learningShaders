b8fe43b0b77023e150cfe298a082d52b3a2f25e9
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-04-13 16:22:04 -0700

    004_gl_FragCoord_demo shows how to access pixel's screen coords

--- 003_uniform_time.glsl
+++ 003_uniform_time.glsl
@@ -1,11 +1,21 @@
 #ifdef GL_ES
 precision mediump float;
 #endif
+/**
+*  shaderToy automatically defines iGlobalTime
+*  for other implementations, uncomment below: 
+*/
 
-// shaderToy automatically defines iGlobalTime
-  // for other implementations, uncomment below: 
-  // uniform float iGlobalTime;
+// uniform float iGlobalTime;
 
 void main() {
 	gl_FragColor = vec4(abs(sin(iGlobalTime)),0.0,0.0,1.0);
 }
+
+// void main() {
+//   gl_FragColor = vec4(
+//     abs(sin(iGlobalTime * 5.0) - 0.3 ),
+//     abs(tan(iGlobalTime * 3.0) - 0.5 ),
+//     abs(cos(iGlobalTime * 2.0) - 0.9),
+//     1.0);
+// }
---
+++ 004_gl_FragCoord_demo.glsl
@@ -0,0 +1,33 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+/**
+*  shaderToy automatically defines iResolution
+*  For other implementations, uncomment below: 
+*/
+
+// uniform vec2 iResolution;
+
+uniform vec2 u_mouse;
+
+void main() {
+	vec2 st = gl_FragCoord.xy/iResolution.xy;
+	gl_FragColor = vec4(st.x,st.y,0.0,1.0);
+}
+
+/** 
+* color vec4(0,0,0,1) is black.
+* thus coordinate (0,0) is bottom-left
+*
+* color vec4(1,0,0,1) is red.
+* thus coordinate (1,0) is bottom-right
+*
+* color vec4(0,1,0,1) is green.
+* thus coordinate (0,) is top-left
+*
+* color vec4(1,1,0,1) is yellow.
+* thus coordinate (1,1) is top-right
+*
+* color vec4(0.5,0.5,0,1) is pukey ylw/grn.
+* thus coordinate (.5,.5) is the middle
+*/
